623f5c3c8310fcda9f479e1d6a54e300bb1c154c
soomin Kang <tnals3256@naver.com>
2024-02-13 12:14:39 +0900

    초기 셋팅 커밋

---
+++ src/test/java/com/mysite/Team_Proj1/TeamProj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.Team_Proj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
